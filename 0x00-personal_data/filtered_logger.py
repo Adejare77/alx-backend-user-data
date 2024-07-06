d9f3178311a5b16dfda6d572da4ca9aaf882079a
Adejare <adejare.arasheed@gmail.com>
2024-07-06 14:16:45 +0100

    Personal Data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -5,7 +5,7 @@ import re
 from typing import List
 import logging
 import os
-import mysql
+import mysql.connector
 
 
 PII_FIELD = ('name', 'email', 'phone', 'ssn', 'password')
@@ -28,34 +28,37 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]) -> None:
-        # super(Child, self).__init__(self.FORMAT)
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
-        # super().__init__(self.FORMAT)
         self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
-        """ returns the log message obfuscated """
-        # message = super().format(record)  # call Parent class's format Method
+        """
+        redact the message of LogRecord instance
+        Args:
+        record (logging.LogRecord): LogRecord instance containing message
+        Return:
+            formatted string
+        """
         message = super(RedactingFormatter, self).format(record)
-        message = filter_datum(self.fields, self.REDACTION, message,
-                               self.SEPARATOR)
-        return message
+        redacted = filter_datum(self.fields, self.REDACTION,
+                                message, self.SEPARATOR)
+        return redacted
 
 
 def get_logger() -> logging.Logger:
-    """ returns logging.Logger object """
-    # create a logger instance
+    """
+    Return a logging.Logger object
+    """
     logger = logging.getLogger("user_data")
     logger.setLevel(logging.INFO)
     logger.propagate = False
 
-    # create a file handler
     handler = logging.StreamHandler()
-    formatter = RedactingFormatter(PII_FIELD)
-    handler.setFormatter(formatter)
 
-    # set the logger
+    formatter = RedactingFormatter(PII_FIELDS)
+
+    handler.setFormatter(formatter)
     logger.addHandler(handler)
     return logger
 
@@ -67,8 +70,10 @@ def get_db() -> mysql.connector.connection.MySQLConnection:
     passwd = os.getenv('PERSONAL_DATA_DB_PASSWORD') or ""
     host = os.getenv('PERSONAL_DATA_DB_HOST') or "localhost"
     db_name = os.getenv('PERSONAL_DATA_DB_NAME')
-    conn = mysql.connector.connect(user=user, password=passwd,
-                                   host=host, database=db_name)
+    conn = mysql.connector.connect(user=user,
+                                   password=passwd,
+                                   host=host,
+                                   database=db_name)
     return conn
 
 
