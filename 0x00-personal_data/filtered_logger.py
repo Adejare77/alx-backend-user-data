31061b2ff2d0eb6223d64cdf26ef9636716cff9a
Adejare <adejare.arasheed@gmail.com>
2024-07-06 12:30:25 +0100

    Personal Data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,6 +3,7 @@
 
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
@@ -12,3 +13,26 @@ def filter_datum(fields: List[str], redaction: str, message: str,
         message = re.sub(fr'(?<={field}=)[^{separator}]+',
                          f'{redaction}', message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, *args, **kwargs):
+        # super(Child, self).__init__(self.FORMAT)
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        # super().__init__(self.FORMAT)
+        self.fields = kwargs.get('fields')
+
+    def format(self, record: logging.LogRecord) -> str:
+        """ returns the log message obfuscated """
+        # message = super(RedactingFormatter, self).format(record)
+        message = super().format(record)  # call Parent class's format Method
+        message = filter_datum(self.fields, self.REDACTION, message,
+                               self.SEPARATOR)
+        return message
