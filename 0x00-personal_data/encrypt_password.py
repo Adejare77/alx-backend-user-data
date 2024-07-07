5b6c828675fb61a7a55dda97d5a2a226413dd25f
Adejare <adejare.arasheed@gmail.com>
2024-07-07 21:29:30 +0100

    Personal Data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -11,3 +11,20 @@ def hash_password(password: str) -> bytes:
     # Hash password using hashpw with salt addition using gensalt()
     hashed = bcrypt.hashpw(passwd, bcrypt.gensalt())
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """validate that the provided password matches the hashed password
+
+    Args:
+        hashed_password (bytes): The hashed password in bytes
+        password (str): The original password before hashing
+
+    Returns:
+        bool: True if the above are equal, else False
+    """
+    # convert original password to bytes
+    passwd = password.encode('utf-8')
+    if bcrypt.checkpw(passwd, hashed_password):
+        return True
+    return False
