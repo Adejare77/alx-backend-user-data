f695066e18b07d4a39359838eb94572ed9e8de95
Adejare <adejare.arasheed@gmail.com>
2024-07-07 10:30:36 +0100

    Personal Data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,7 +31,7 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self, fields: List[str]):
+    def __init__(self, fields: List[str]) -> None:
         """Initialize the parent class with the format and
         fields to be redacted"""
         super(RedactingFormatter, self).__init__(self.FORMAT)
