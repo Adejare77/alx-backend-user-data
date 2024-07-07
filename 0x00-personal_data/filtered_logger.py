142343d2e211be6f688e95e399f3b258aebeb7d1
Adejare <adejare.arasheed@gmail.com>
2024-07-07 10:25:38 +0100

    Personal Data

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """Regex-ing"""
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -20,3 +21,35 @@ def filter_datum(fields: List[str], redaction: str,
         pattern = rf'(?<={field}=)[^{separator}]+'
         message = re.sub(pattern, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]) -> None:
+        """Initialize the parent class with the format and
+        fields to be redacted"""
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        """filter values in incoming log records using filter_datum.
+        values for the fields should be filtered
+
+        Args:
+            record (logging.LogRecord): Incoming log record. Created using
+            logging.LogRecord(...). Contains message, name etc needed
+
+        Returns:
+            str: obfuscated message
+        """
+        formatted_msg = super().format(record)
+        obfuscated_msg = filter_datum(self.fields, self.REDACTION,
+                                      formatted_msg, self.SEPARATOR)
+
+        return obfuscated_msg
