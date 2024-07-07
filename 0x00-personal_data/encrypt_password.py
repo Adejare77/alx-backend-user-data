cac3073514201f0a3777c7300a59e0e004d9ed40
Adejare <adejare.arasheed@gmail.com>
2024-07-07 21:23:14 +0100

    Personal Data

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,29 +1,13 @@
 #!/usr/bin/env python3
-"""
-Defines a hash_password function to return a hashed password
-"""
+"""Encrypting passwords using bcrypt."""
+
 import bcrypt
-from bcrypt import hashpw
 
 
 def hash_password(password: str) -> bytes:
-    """
-    Returns a hashed password
-    Args:
-        password (str): password to be hashed
-    """
-    b = password.encode()
-    hashed = hashpw(b, bcrypt.gensalt())
+    """ returns a salted, hashed password, which is a byte string """
+    # convert password to bytes
+    passwd = password.encode('utf-8')
+    # Hash password using hashpw with salt addition using gensalt()
+    hashed = bcrypt.hashpw(passwd, bcrypt.gensalt())
     return hashed
-
-
-def is_valid(hashed_password: bytes, password: str) -> bool:
-    """
-    Check whether a password is valid
-    Args:
-        hashed_password (bytes): hashed password
-        password (str): password in string
-    Return:
-        bool
-    """
-    return bcrypt.checkpw(password.encode(), hashed_password)
