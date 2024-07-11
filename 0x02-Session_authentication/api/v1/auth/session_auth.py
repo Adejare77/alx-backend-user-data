4a3cbe5138841f969861b59e3e7177152c1dd0a8
Adejare <adejare.arasheed@gmail.com>
2024-07-11 14:24:09 +0100

    Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 
 from api.v1.auth.auth import Auth
 import uuid
+from typing import TypeVar
 
 
 class SessionAuth(Auth):
@@ -16,3 +17,13 @@ class SessionAuth(Auth):
         session_id = str(uuid.uuid4())
         self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None):
+        """ returns a User ID based on a Session ID given
+
+        Args:
+            session_id (str, optional): Session ID. Defaults to None.
+        """
+        if not (session_id and type(session_id) is str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
