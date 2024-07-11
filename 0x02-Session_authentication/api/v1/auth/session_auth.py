a692f4bfe139c2095ad11792147cbbc01585a528
Adejare <adejare.arasheed@gmail.com>
2024-07-11 12:55:03 +0100

    Session Authentication

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,8 +2,17 @@
 """ Manage the Session authentication """
 
 from api.v1.auth.auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """ Use Session Authentication """
-    pass
+    user_id_by_session_id = {}  # "in-memory session ID storage"
+
+    def create_session(self, user_id: str = None) -> str:
+        """ create a Session ID for a user """
+        if not (user_id and type(user_id) is str):
+            return None
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
