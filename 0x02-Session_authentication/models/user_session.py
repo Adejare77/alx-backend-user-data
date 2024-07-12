f1d8aeef9f1b4cde6d1eb963c1755ff9dec05417
Adejare <adejare.arasheed@gmail.com>
2024-07-12 12:37:15 +0100

    Models

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -9,5 +9,5 @@ class UserSession(Base):
     """
     def __init__(self, *args: list, **kwargs: dict):
         super(UserSession, self).__init__(*args, **kwargs)
-        self.user = kwargs.get('user_id')
+        self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')
