8416db0c6986772f989ad39aa189c3fab251c194
Adejare <adejare.arasheed@gmail.com>
2024-07-12 12:27:13 +0100

    Models

---
+++ 0x02-Session_authentication/models/user_session.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Stores all Session IDs as a file """
+from models.base import Base
+
+
+class UserSession(Base):
+    """ New authentication system, based on Session ID stored in
+    database
+    """
+    def __init__(self, *args: list, **kwargs: dict):
+        super(UserSession, self).__init__(*args, **kwargs)
+        self.user = kwargs.get('user_id')
+        self.session_id = kwargs.get('session_id')
