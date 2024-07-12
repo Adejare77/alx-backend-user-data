fe9acd82ee9478a18ee0fa9455d42b7133b9bd66
Adejare <adejare.arasheed@gmail.com>
2024-07-12 12:40:24 +0100

    Models

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """ Stores all Session IDs as a file """
 from models.base import Base
+import uuid
 
 
 class UserSession(Base):
@@ -9,5 +10,5 @@ class UserSession(Base):
     """
     def __init__(self, *args: list, **kwargs: dict):
         super(UserSession, self).__init__(*args, **kwargs)
-        self.user_id = kwargs.get('user_id')
-        self.session_id = kwargs.get('session_id')
+        self.user_id = kwargs.get('user_id', str(uuid.uuid4()))
+        self.session_id = kwargs.get('session_id', str(uuid.uuid4()))
